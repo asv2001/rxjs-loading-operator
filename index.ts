79e5b805159b191eab22bd0fd6db35da05541c5d
asi <anton.sinelnyk@smarter-ecommerce.com>
2020-07-24 09:13:42 +0200

    Fix types issue

--- index.ts
+++ index.ts
@@ -3,22 +3,23 @@ import { MonoTypeOperatorFunction, Observable, Subscriber, Subscription } from "
 export type LoadingCallback = (state: boolean) => void;
 
 export function loadingState<T>(action: LoadingCallback): MonoTypeOperatorFunction<T> {
-  return <T>(source: Observable<T>) => {
-    return new Observable<T>((observer: Subscriber<T>): Subscription => {
-      action(true);
-      return source.subscribe({
-        next(x) {
-          observer.next(x);
-        },
-        error(err) {
-          observer.error(err);
-          action(false);
-        },
-        complete() {
-          observer.complete();
-          action(false);
-        }
-      });
-    });
-  };
+  return (source: Observable<T>) =>
+    new Observable<T>(
+      (observer: Subscriber<T>): Subscription => {
+        action(true);
+        return source.subscribe({
+          next(x) {
+            observer.next(x);
+          },
+          error(err) {
+            observer.error(err);
+            action(false);
+          },
+          complete() {
+            observer.complete();
+            action(false);
+          }
+        });
+      }
+    );
 }
